d0281c328bf47db6bbbaedd959d549a9f0f4b570
fox_itleksandr <fox_it@bk.ru>
2022-01-18 15:13:57 +0300

    Add project files.

---
+++ Lab17/BancAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    public class BankAccount<T>
+    {
+        private T _number;
+        private double _balance;
+        private string _fio;
+
+
+        public BankAccount(T Number, string FIO, double Balance)
+        {
+            _number = Number;
+            _fio = FIO;
+            _balance = Balance;
+        }
+
+        public void SetAccount(T Number, string FIO, double Balance)
+        {
+            _number = Number;
+            _fio = FIO;
+            _balance = Balance;   
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Счёт №{0}\n  Владелец: {1}, Баланс: {2:f2}\n", _number,_fio,_balance);
+        }
+
+
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BankAccount<int> baInt = new BankAccount<int>(1, "Владимир Ильич Ленин", 0);
+            BankAccount<string> baSrting = new BankAccount<string>("007", "Бонд Джеймс Бондович", 100.01);
+
+            baInt.GetInfo();
+            baSrting.GetInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+}
